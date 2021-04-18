4b60d0223ad5b1c20936859ece99126ca600de48
jpyatachkov <cvkucherov@yandex.ru>
2021-04-18 15:58:04 +0300

    Update class-transformer

--- dist/decorators/size.decorator.d.ts
+++ dist/decorators/size.decorator.d.ts
@@ -3,4 +3,4 @@ export interface SizeOptions {
     maxSize: number;
     sizeParam: string;
 }
-export declare const Size: (...dataOrPipes: (SizeOptions | import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>>)[]) => ParameterDecorator;
+export declare const Size: (...dataOrPipes: (import("@nestjs/common").PipeTransform<any, any> | import("@nestjs/common").Type<import("@nestjs/common").PipeTransform<any, any>> | SizeOptions)[]) => ParameterDecorator;
